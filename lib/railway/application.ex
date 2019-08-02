4519952bb4866e4a9f5251d6c2554459042035d6
Carlo Gilmar <carlogilmar12@gmail.com>
2019-08-01 21:09:52 -0500

    First commit dude!

---
+++ lib/railway/application.ex
@@ -0,0 +1,19 @@
+defmodule Railway.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Railway.Worker.start_link(arg)
+      # {Railway.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Railway.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
